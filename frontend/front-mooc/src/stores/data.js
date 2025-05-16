a1a8d3b77c2a285c595b5c178f24f4b90319db89
promise-temi <temi.promisejohn@gmail.com>
2025-05-16 17:19:42 +0200

    ajout de la persistance des états meme apres rachaichissement de la page

--- frontend/front-mooc/src/stores/data.js
+++ frontend/front-mooc/src/stores/data.js
@@ -2,5 +2,6 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', {
   state: () => ({ 
     connected: false,
-})
+}),
+persist: true // <-- C'est ça la magie !
 })
