319a2a0c562aa9ebfd05913783f26f5ada8f8c56
promise-temi <temi.promisejohn@gmail.com>
2025-05-19 16:52:36 +0200

    enrichissement de la barre de navigation avec des liens

--- frontend/front-mooc/src/components/BarreDeNavigation.vue
+++ frontend/front-mooc/src/components/BarreDeNavigation.vue
@@ -3,13 +3,20 @@
     <header class="navbar-header">
         <div class="navbar-container">
             <h1 class="navbar-title">Mooc</h1>
-            <div class="deconnexion" >
-            
-                <svg :onclick="deconnexion" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
-                </svg>
-            </div>
+            <nav>
+                <ul>
+                        <li><router-link to="/">Accueil</router-link></li>
+                        <li><router-link to="/">Topics</router-link></li>
+                        <li><router-link to="/">Users</router-link></li>
+                    </ul>
+                <div class="deconnexion" >
+                    
+                    <svg :onclick="deconnexion" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                    </svg>
+                </div>
+            </nav>
         </div>
     </header>
 </template>
@@ -63,4 +70,18 @@ header.navbar-header {
     
 }
 
+nav ul{
+    display: flex;
+    list-style: none;
+    gap: 20px;
+    margin: 0 50px;
+
+}
+
+nav ul li{
+    text-decoration: none!important;
+}
+nav {
+    display: flex;
+}
 </style>
